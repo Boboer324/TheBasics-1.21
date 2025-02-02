4b6b70c83c330230bcf34bec806c4955a7a2214a
Boboer324 <HAThies@gmail.com>
2025-02-02 16:34:47 +0100

    ok

---
+++ src/main/java/bobo/basic/TheBasicsClient.java
@@ -0,0 +1,10 @@
+package bobo.basic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBasicsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bobo/basic/TheBasicsDataGenerator.java
@@ -0,0 +1,11 @@
+package bobo.basic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheBasicsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
